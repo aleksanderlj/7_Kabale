428a0d0c2a19fd9b4855251f953acc7fb5bd509c
Jonatan <Jonatandahl99@gmail.com>
2020-04-01 11:05:27 +0200

    Continued work on the cards.

--- app/src/main/java/com/example/a7_kabale/logic/Card.java
+++ app/src/main/java/com/example/a7_kabale/logic/Card.java
@@ -5,6 +5,8 @@ import androidx.annotation.NonNull;
 public class Card {
 	private int value;
 	private String suit;
+	// This value is only meant for cards in the stackdecks
+	private boolean shown;
 	
 	public Card (int value, String suit){
 		this.value = value;
@@ -15,5 +17,13 @@ public class Card {
 	@Override
 	public String toString() {
 		return this.value + this.suit;
+	}
+	
+	public boolean isShown() {
+		return shown;
+	}
+	
+	public void setShown(boolean shown) {
+		this.shown = shown;
 	}
 }
--- app/src/main/java/com/example/a7_kabale/logic/GameEngine.java
+++ app/src/main/java/com/example/a7_kabale/logic/GameEngine.java
@@ -4,33 +4,47 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GameEngine {
-	private List<Card> cardDeck;
+	private List<Card> allCards;
+	private List<Card>[] aceDecks;
+	private List<Card>[] cardDecks;
+	private List<Card>[] stackDecks;
+	private int hiddenCards;
+	private Card shownCard;
 	
-	public void initiateGame(){
-		cardDeck = initiateCardDeck();
-		for (Card card : cardDeck){
-			System.out.println(card);
-		}
-	}
-	
-	private List<Card> initiateCardDeck(){
-		List<Card> cardDeck = new ArrayList<>();
+	private void initiateGame(){
+		initiateCards();
+		//setGameState();
 		
-		for(int i = 0; i < 52; i++){
-			if (i < 13){
-				cardDeck.add(new Card((i % 13), "Hearts"));
-			}
-			else if (i < 26){
-				cardDeck.add(new Card(i % 13, "Diamonds"));
-			}
-			else if (i < 39){
-				cardDeck.add(new Card(i % 13, "Clubs"));
-			}
-			else{
-				cardDeck.add(new Card(i % 13, "Spades"));
-			}
-		}
-		return cardDeck;
 	}
 	
+	
+	private void initiateCards(){
+		allCards = new ArrayList<>();
+		for(int i = 0; i < 52; i++){
+			if (i < 13)
+				allCards.add(new Card((i % 13), "Hearts"));
+			else if (i < 26)
+				allCards.add(new Card(i % 13, "Diamonds"));
+			else if (i < 39)
+				allCards.add(new Card(i % 13, "Clubs"));
+			else
+				allCards.add(new Card(i % 13, "Spades"));
+		}
+		aceDecks[0] = new ArrayList<>();
+		aceDecks[1] = new ArrayList<>();
+		aceDecks[2] = new ArrayList<>();
+		aceDecks[3] = new ArrayList<>();
+		stackDecks[0] = new ArrayList<>();
+		stackDecks[1] = new ArrayList<>();
+		stackDecks[2] = new ArrayList<>();
+		stackDecks[3] = new ArrayList<>();
+		stackDecks[4] = new ArrayList<>();
+		stackDecks[5] = new ArrayList<>();
+		stackDecks[6] = new ArrayList<>();
+	}
+	
+	// TODO - This must be implemented, when we get data from a picture
+	private void setGameState(){
+	
+	}
 }
