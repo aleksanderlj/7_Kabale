993f201d3f2d5494f8105822970bc464ad350bda
Kris <kris@kris.kris>
2020-04-08 13:06:35 +0200

    Template matching uden procent

--- app/src/main/java/com/example/a7_kabale/CardRecognizer.java
+++ app/src/main/java/com/example/a7_kabale/CardRecognizer.java
@@ -11,17 +11,39 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import org.opencv.android.Utils;
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
 
 public class CardRecognizer {
         private Activity callingActivity;
         private ImageView screenImage;
         private int currentImage = 1;
-        private Mat imageMat, editedImageMat;
         private Bitmap imgBitmap;
+        private Bitmap spades, hearts, diamonds, clubs;
+        private Mat imageMat, editedImageMat;
+        private Mat spadesMat, heartsMat, diamondsMat, clubsMat;
+        private int matchMethod=Imgproc.TM_CCOEFF;
 
         public CardRecognizer(Activity activity) {
             callingActivity = activity;
+
+            //Init openCV
+            editedImageMat = new Mat();
+            spadesMat = new Mat();
+            heartsMat = new Mat();
+            diamondsMat = new Mat();
+            clubsMat = new Mat();
+            spades = BitmapFactory.decodeResource(callingActivity.getResources(), R.mipmap.spades);
+            hearts = BitmapFactory.decodeResource(callingActivity.getResources(), R.mipmap.hearts);
+            diamonds = BitmapFactory.decodeResource(callingActivity.getResources(), R.mipmap.diamonds);
+            clubs = BitmapFactory.decodeResource(callingActivity.getResources(), R.mipmap.clubs);
+            Utils.bitmapToMat(spades, spadesMat);
+            Utils.bitmapToMat(hearts, heartsMat);
+            Utils.bitmapToMat(diamonds, diamondsMat);
+            Utils.bitmapToMat(clubs, clubsMat);
         }
 
         public void doThings() {
@@ -67,9 +89,14 @@ public class CardRecognizer {
                 TextView text = (TextView) callingActivity.findViewById(R.id.textView);
 
                 int color = getCardColor();
-                result = colorIdToString(color);
+                result = getCardSuit(color);
+                result += " " + colorIdToString(color);
                 result += " " + getCardValue();
-                result += " " + getCardSuit(color);
+
+                Mat tmp = new Mat();
+                Bitmap bmp = Bitmap.createBitmap(tmp.cols(), tmp.rows(), Bitmap.Config.ARGB_8888);
+                Utils.matToBitmap(tmp, bmp);
+                screenImage.setImageBitmap(bmp);
 
                 text.setText(result);
             }
@@ -89,6 +116,12 @@ public class CardRecognizer {
     private String getCardSuit(int color) {
         //Her kan det være en god idé at matche kuløren ud fra et andet billede med OpenCVs template matching.
         // Det største match må være kuløren.
+        Imgproc.matchTemplate(imageMat, heartsMat, editedImageMat, matchMethod);
+        Core.MinMaxLocResult mmr = Core.minMaxLoc(editedImageMat);
+        Point matchLoc=mmr.maxLoc;
+        Imgproc.rectangle(imageMat, matchLoc, new Point(matchLoc.x + heartsMat.cols(),
+                matchLoc.y + heartsMat.rows()), new Scalar(255, 255, 255));
+
         return "Spades";
     }
 
