458f40b3f88d1ab3ffa4ebf6661026812599d9d8
Jonatan <Jonatandahl99@gmail.com>
2020-06-09 13:28:49 +0200

    Added more datatypes.

--- app/src/main/java/com/example/a7_kabale/logic/LogicState.java
+++ app/src/main/java/com/example/a7_kabale/logic/LogicState.java
@@ -1,10 +1,13 @@
 package com.example.a7_kabale.logic;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class LogicState {
 	private ArrayList<ArrayList<Card>> tableauRows;
 	private Card topDeckCard, foundationsDeckDiamonds, foundationsDeckHearts, foundationsDeckClubs, foundationsDeckSpades;
+	private int shownCards;
+	private List<Integer> hiddenCards = new ArrayList<>();
 	
 	public LogicState(ArrayList<ArrayList<Card>> tableauRows, Card topDeckCard, Card foundationsDeckDiamonds, Card foundationsDeckHearts,
 					  Card foundationsDeckClubs, Card foundationsDeckSpades){
@@ -14,6 +17,14 @@ public class LogicState {
 		this.foundationsDeckHearts = foundationsDeckHearts;
 		this.foundationsDeckClubs = foundationsDeckClubs;
 		this.foundationsDeckSpades = foundationsDeckSpades;
+		shownCards = 7;
+		hiddenCards.add(0);
+		hiddenCards.add(1);
+		hiddenCards.add(2);
+		hiddenCards.add(3);
+		hiddenCards.add(4);
+		hiddenCards.add(5);
+		hiddenCards.add(6);
 	}
 	
 	public ArrayList<ArrayList<Card>> getTableauRows() {
@@ -62,5 +73,21 @@ public class LogicState {
 	
 	public void setFoundationsDeckSpades(Card foundationsDeckSpades) {
 		this.foundationsDeckSpades = foundationsDeckSpades;
+	}
+	
+	public int getShownCards() {
+		return shownCards;
+	}
+	
+	public void setHiddenCards(List<Integer> hiddenCards) {
+		this.hiddenCards = hiddenCards;
+	}
+	
+	public List<Integer> getHiddenCards() {
+		return hiddenCards;
+	}
+	
+	public void setShownCards(int shownCards) {
+		this.shownCards = shownCards;
 	}
 }
